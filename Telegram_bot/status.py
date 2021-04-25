03d7e34c86d98cd69acdbf80acb5d173696387d9
vvburkovsky <bvitaliy1@gmail.com>
2021-04-26 00:34:33 +0300

    Add get wifi status

--- Telegram_bot/status.py
+++ Telegram_bot/status.py
@@ -2,8 +2,8 @@ def state ():
     file = open('/usr/bin/Telegram_bot/status_wifi.txt', encoding='utf-8')
     lines = file.read().splitlines()    
     try:
-        line1 = str (lines[0])
-        line2 = str (lines[1])
+        line1 = str(lines[0])
+        line2 = str(lines[1])
         status = (line1 + '\n' + line2)
         return status
     except:
--- Telegram_bot/status_wifi.sh
+++ Telegram_bot/status_wifi.sh
@@ -4,11 +4,13 @@ cd /usr/bin/Telegram_bot #Bot Directory
 uci show wireless.radio0 | egrep 'txpower'\|'disabled' | tr -d \' > status_wifi.txt #Create File
 sleep 1s
 
-wifi_on=$(grep -w 'wireless.radio0.disabled=0' status_wifi.txt)
+#поиск строки в файле и назначение переменной:
+wifi_on=$(grep -w 'wireless.radio0.disabled=0' status_wifi.txt) 
 wifi_off=$(grep -w 'wireless.radio0.disabled=1' status_wifi.txt)
 min=$(grep -w 'wireless.radio0.txpower=3' status_wifi.txt)
 max=$(grep -w 'wireless.radio0.txpower=21' status_wifi.txt)
 
+#замена строк:
 if [[ $wifi_off == 'wireless.radio0.disabled=1' ]]
     then
         uci show wireless.radio0 | egrep 'disabled' | tr -d \' > status_wifi.txt
